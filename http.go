037afa4559de26ccc705b88dfd094861824f2912
MengXinxin1 <1656746727@qq.com>
2023-03-07 18:12:09 +0800

    update http.go

---
+++ http.go
@@ -0,0 +1,58 @@
+package Cache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_cache/"
+
+// HTTPPool 是承载节点间http通信最核心的数据结构
+type HTTPPool struct {
+	self     string
+	basePath string
+}
+
+// NewHttpPool 中有两个参数 self用来记录自己的地址，basePath是节点之间通信的前缀
+func NewHttpPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+// ServerHTTP首先判断前缀是否含有basePath,约定访问路径为
+// /<basePath>/<groupName>/<key>通过groupName得到group实例，
+// 再使用group.Get(key)获取缓存数据
+func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basePath>/<groupName>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	groupName := parts[0]
+	key := parts[1]
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
