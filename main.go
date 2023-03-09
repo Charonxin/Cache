e2ba2df188a7d3b2226d4683f8d150d7a146a321
MengXinxin1 <1656746727@qq.com>
2023-03-09 15:47:38 +0800

    update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Cache/cache"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,16 +14,63 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
-func main() {
-	cache.NewGroup("scores", 2<<10, cache.GetterFunc(func(key string) ([]byte, error) {
-		log.Println("[slowDB] serch key", key)
-		if v, ok := db[key]; ok {
-			return []byte(v), nil
-		}
-		return nil, fmt.Errorf("%s not exist", key)
-	}))
-	addr := "localhost:9999"
+func createGroup() *cache.Group {
+	return cache.NewGroup("scores", 2<<10, cache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+}
+
+func startCacheServer(addr string, addrs []string, gee *cache.Group) {
 	peers := cache.NewHttpPool(addr)
-	log.Println("cache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, gee *cache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "Geecache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gee := createGroup()
+	if api {
+		go startAPIServer(apiAddr, gee)
+	}
+	startCacheServer(addrMap[port], []string(addrs), gee)
 }
